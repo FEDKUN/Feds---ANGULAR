cfde17861e266cd347f9e01d0bef832d89e847fb
feds <federico.poleros22@gmail.com>
2024-03-02 14:59:33 +0800

    PORTFOLIO

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodoComponent } from './todo/todo.component';
+import { HeroComponent } from './hero/hero.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'todo', component:TodoComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
